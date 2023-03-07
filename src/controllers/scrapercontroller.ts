df1f3b0ea23936a9d3c34d14bdfa707dd9502ad0
miolfo <mikko.o.forsman@gmail.com>
2023-03-07 17:57:37 +0200

    Example of getting a finna title from finna url with selenium headless mode

--- src/controllers/scrapercontroller.ts
+++ src/controllers/scrapercontroller.ts
@@ -1,16 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
-import {WebDriver, Builder, Key, By, until} from "selenium-webdriver";
-
+import {Builder, By} from "selenium-webdriver";
+import firefox from "selenium-webdriver/firefox"
 export const getData = async (req: Request, res: Response, next: NextFunction) => {
 
-    let driver = await new Builder().forBrowser("firefox").build();
+    let driver = await new Builder()
+        .forBrowser("firefox")
+        .setFirefoxOptions(new firefox.Options().headless().windowSize({
+            width: 640,
+            height: 480
+        }))
+        .build();
+    let text;
     try {
-        await driver.get('http://www.google.com/ncr');
-        await driver.findElement(By.name('q')).sendKeys('You did it!!', Key.RETURN);
+        await driver.get('https://www.finna.fi/Record/helmet.2527262');
+        let test = await driver.findElement(By.className('record-title visible-xs'));
+        console.log(test)
+        text = await test.getText()
     } finally {
         await driver.quit();
     }
     return res.status(200).json({
-        message: "Hello world"
+        message: text
     });
 };
