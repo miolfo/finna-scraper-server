dab12fa7cbe41c528afb6fbf7748b3d384cd213f
miolfo <mikko.o.forsman@gmail.com>
2023-03-07 20:02:59 +0200

    Parse book status results into status enum

--- src/controllers/ScraperController.ts
+++ src/controllers/ScraperController.ts
@@ -1,11 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-import {Builder, By} from "selenium-webdriver";
-import firefox from "selenium-webdriver/firefox"
 import {checkAvailability} from "../scraper/Scraper";
 export const getAvailability = async (req: Request, res: Response, next: NextFunction) => {
 
     let text = await checkAvailability('https://www.finna.fi/Record/helmet.1661329')
     return res.status(200).json({
-        message: text
+        statuses: text
     });
 };
--- src/scraper/Scraper.ts
+++ src/scraper/Scraper.ts
@@ -1,7 +1,27 @@
 import {Builder, By, until} from "selenium-webdriver";
 import firefox from "selenium-webdriver/firefox";
 
-export const checkAvailability = async (finnaUrl: string) => {
+type Status = 'ORDERED' | 'UNAVAILABLE' | 'AVAILABLE' | 'WAITING' | 'IN_TRANSIT' | 'UNKNOWN'
+
+
+//TODO: Include amount available, or closest return date in return type objects
+const parseStatus = (holdingsText: string): Status => {
+    if(holdingsText.toLowerCase().includes('tilattu')) {
+        return 'ORDERED';
+    } else if(holdingsText.toLowerCase().includes('odottaa noutoa')) {
+        return 'WAITING';
+    } else if(holdingsText.toLowerCase().includes('saatavissa')) {
+        return 'AVAILABLE';
+    } else if(holdingsText.toLowerCase().includes('kuljetuksessa')) {
+        return 'IN_TRANSIT';
+    } else if(holdingsText.toLowerCase().includes('eräpäivä')) {
+        return 'UNAVAILABLE';
+    } else {
+        return 'UNKNOWN';
+    }
+}
+
+export const checkAvailability = async (finnaUrl: string): Promise<Status[]> => {
     let driver = await new Builder()
         .forBrowser("firefox")
         .setFirefoxOptions(new firefox.Options().headless().windowSize({
@@ -9,18 +29,18 @@ export const checkAvailability = async (finnaUrl: string) => {
             height: 480
         }))
         .build();
-    let text = '';
+    let statuses: Status[] = [];
     try {
         await driver.get(finnaUrl);
         //Wait until holdings- info is loaded
-        let holdingsTitle = await driver.wait(until.elementLocated(By.className('holdings-title')),10000);
+        await driver.wait(until.elementLocated(By.className('holdings-title')),10000);
         let allHoldings = await driver.wait(until.elementsLocated(By.xpath('//div[contains(@class, \'holdings-details\')]/span')), 10000);
         for (const element of allHoldings) {
-            const value = await element.getText()
-            text += value + ", "
+            const value = await element.getText();
+            statuses.push(parseStatus(value));
         }
     } finally {
         await driver.quit();
     }
-    return text;
+    return statuses;
 }
