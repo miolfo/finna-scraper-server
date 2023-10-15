e81914515b7da2bc618641b63294fd066f3ac6ce
miolfo <mikko.o.forsman@gmail.com>
2023-10-15 19:18:40 +0300

    Don't reinitialize webdriver for every request

--- src/scraper/Scraper.ts
+++ src/scraper/Scraper.ts
@@ -1,4 +1,4 @@
-import {Builder, By, until} from "selenium-webdriver";
+import {Builder, By, until, WebDriver} from "selenium-webdriver";
 import firefox from "selenium-webdriver/firefox";
 
 type Status = 'ORDERED' | 'UNAVAILABLE' | 'AVAILABLE' | 'WAITING' | 'IN_TRANSIT' | 'UNKNOWN'
@@ -22,25 +22,30 @@ const parseStatus = (holdingsText: string): Status => {
 }
 
 export const checkAvailability = async (finnaUrl: string): Promise<Status[]> => {
-    let driver = await new Builder()
-        .forBrowser("firefox")
-        .setFirefoxOptions(new firefox.Options().headless().windowSize({
-            width: 640,
-            height: 480
-        }))
-        .build();
+
     let statuses: Status[] = [];
-    try {
-        await driver.get(finnaUrl);
-        //Wait until holdings- info is loaded
-        await driver.wait(until.elementLocated(By.className('holdings-title')),10000);
-        let allHoldings = await driver.wait(until.elementsLocated(By.xpath('//div[contains(@class, \'holdings-details\')]/span')), 10000);
-        for (const element of allHoldings) {
-            const value = await element.getText();
-            statuses.push(parseStatus(value));
-        }
-    } finally {
-        await driver.quit();
+    const driver = await getDriver();
+    await driver.get(finnaUrl);
+    //Wait until holdings- info is loaded
+    await driver.wait(until.elementLocated(By.className('holdings-title')),10000);
+    let allHoldings = await driver.wait(until.elementsLocated(By.xpath('//div[contains(@class, \'holdings-details\')]/span')), 10000);
+    for (const element of allHoldings) {
+        const value = await element.getText();
+        statuses.push(parseStatus(value));
     }
     return statuses;
+}
+
+let driver: WebDriver | undefined = undefined
+const getDriver = async (): Promise<WebDriver> => {
+    if (!driver) {
+        driver = await new Builder()
+            .forBrowser("firefox")
+            .setFirefoxOptions(new firefox.Options().headless().windowSize({
+                width: 640,
+                height: 480
+            }))
+            .build();
+    }
+    return driver
 }
